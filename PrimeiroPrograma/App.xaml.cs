ab12ca8fb82677270759e03bb57905a55b34fdaa
Vitto633 <vitor.santos633@etec.sp.gov.br>
2024-03-02 21:08:48 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroPrograma/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeiroPrograma
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PrimeiroPrograma/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeiroPrograma
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
